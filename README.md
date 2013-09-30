684f6f2bd138aabe8e3714cae72c22d741304e74
Ryan Hewitt <ryan@mesuva.com.au>
2013-09-30 21:57:06 +0930

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Overview
+==========
+A concrete5 toolbar to quickly jump to editing a eCommerce product, from viewing the product's page.
+This saves having to go to the dashboard and searching for the corresponding product.
+
+Installation
+==========
+Place the folder *manage_product_button* (rename if necessary) in the top level *packages* folder of your concrete5 site and install via the Dashboard install function. No other install steps are required. 
+
+The button will automatically display on the toolbar on product pages when logged in.
+
+## Requires 
+Requires at least concrete5 version 5.4.1.1 and the eCommerce add-on.
---
+++ controller.php
@@ -0,0 +1,42 @@
+<?php
+defined('C5_EXECUTE') or die(_("Access Denied."));
+
+class ManageProductButtonPackage extends Package {
+
+	protected $pkgHandle = 'manage_product_button';
+	protected $appVersionRequired = '5.5.1';
+	protected $pkgVersion = '0.9';
+	
+	public function getPackageDescription() {
+		return t("A toolbar button to quickly manage an eCommerce product.");
+	}
+	
+	public function getPackageName() {
+		return t("Manage eCommerce Product Toolbar Button");
+	}
+	
+	public function install() {
+		$installed = Package::getInstalledHandles();
+		
+		if( !(is_array($installed) && in_array('core_commerce',$installed)) ) {
+			throw new Exception(t('This package requires that the <a href="http://www.concrete5.org/marketplace/addons/ecommerce/" target="_blank">eCommerce package</a> is installed<br/>'));	
+		}
+		
+		$pkg = parent::install();	 	
+	}
+	
+	public function on_start() {
+	  if(version_compare(APP_VERSION,'5.4.1.1', '>')){
+		   $ihm = Loader::helper('concrete/interface/menu');
+		   $uh = Loader::helper('concrete/urls');
+		   $req = Request::get(); 
+		   $id = $req->getRequestCollectionID();
+		   $db = Loader::db();
+		   $currentProduct = $db->getOne('select productID from CoreCommerceProducts where cID = ?', $id);
+			
+			if ($currentProduct > 0) {	  	
+		   		$ihm->addPageHeaderMenuItem(null, t('Manage Product'), 'left', array('style'=>' background-position: -15px -2212px !important;','class'=>'ccm-menu-icon', 'href' => '/index.php/dashboard/core_commerce/products/search/view_detail/' .  $currentProduct), 'manage_product_button');
+			}
+	 	}   
+	}
+}
